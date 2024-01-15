59d27a1f33c588f9d9343e4bb5aa0b810f7c9aa4
Tomislav Pavkovic <tomislav.pavkovic@tum.de>
2024-01-15 17:21:41 -0500

    adapt extract_label for generated ct data and add total segmentator labeling

--- preprocessing/extract_label.py
+++ preprocessing/extract_label.py
@@ -15,56 +15,63 @@ def extract_label(label, file_path, new_file_path):
         nib.save(new_image, new_file_path)
 
 if __name__ == "__main__":
-    root_directory = "/home/tomislav/datasets-ct/dataset-CACTS"
+    root_directory = "/home/tomislav/datasets-ct/dataset-CACTS-generated"
     dataset_name = "datasets-ct"
-    new_dataset_name = "datasets-ct-extracted"
+    new_dataset_name = "datasets-ct-extracted-generated"
+    label = 2
+    organ_name = 'right kidney'
+    generated = True
     new_root_directory = root_directory.replace(dataset_name, new_dataset_name)
+
     if not os.path.exists(new_root_directory):
         os.makedirs(new_root_directory)
-    label = 5
-    organ_name = 'liver'
+    
     for root, dirs, files in os.walk(root_directory):
-        for dir in dirs:
-            if dir.startswith("00"):
-                print(dir)
-                summary_filename = root + '/' + dir + '/dataset_summary.xlsx'
-                if not os.path.exists(summary_filename):
-                    continue
-                summary_excel = pd.read_excel(summary_filename, engine='openpyxl')
-                if 'background' in summary_excel.columns:
-                    background_col_num = summary_excel.columns.get_loc('background')
-                    end_index = summary_excel.columns.get_loc('organs_sum')
-                    for organ in summary_excel.columns[background_col_num + 1 : end_index]:
-                        print(organ)
-                        organ_name = organ.lower()
-                        organ_col_num = summary_excel.columns.get_loc(organ)
-                        organ_label = organ_col_num - background_col_num
-                        organ_col = summary_excel[organ]
-                        for index, id in enumerate(summary_excel['ids']):
-                            if organ_col[index] == 1:
-                                if id.startswith('LITS_test-volume'):
-                                    file = dir + '/labels/' + id + '_0000.nii.gz'
-                                else:
-                                    file = dir + '/labels/' + id + '.nii.gz'
-                                file_path = os.path.join(root, file)
-                                new_root = root.replace(dataset_name, new_dataset_name)
-                                new_root = new_root.replace('CACTS', 'CACTS/'+organ_name)
-                                base, extension = os.path.splitext(file)
-                                base2, extension2 = os.path.splitext(base)
-                                new_filename = f"{base2}_{organ_name}{extension2}{extension}"
-                                new_file_path = os.path.join(new_root, new_filename)
-                                if os.path.exists(new_file_path):
-                                    print(new_file_path)
-                                else:
-                                    extract_label(organ_label, file_path, new_file_path)
-
-        '''for file in files:
-            if file.endswith("_part_251.nii.gz"):
-                file_path = os.path.join(root, file)
-                new_root = root.replace(dataset_name, new_dataset_name)
-                base, extension = os.path.splitext(file)
-                base2, extension2 = os.path.splitext(base)
-                new_filename = f"{base2}_{label}{extension2}{extension}"
-                new_file_path = os.path.join(new_root, new_filename)
-                extract_label(label, file_path, new_file_path)
-                '''
+        if generated:
+            for file in files:
+                if file.endswith("_combined.nii.gz"):
+                    print(file)
+                    file_path = os.path.join(root, file)
+                    new_root = root.replace(dataset_name, new_dataset_name)
+                    new_root = new_root.replace('dataset-CACTS-generated', 'dataset-CACTS-generated/' + organ_name)
+                    new_root = new_root.replace('images', 'labels')
+                    new_root = '/'.join(new_root.split('/')[:-1])
+                    #base, extension = os.path.splitext(file)
+                    #base2, extension2 = os.path.splitext(base)
+                    new_filename = file.replace('combined', organ_name)
+                    new_file_path = os.path.join(new_root, new_filename)
+                    extract_label(label, file_path, new_file_path)
+        else:
+            for dir in dirs:
+                if dir.startswith("00"):
+                    print(dir)
+                    summary_filename = root + '/' + dir + '/dataset_summary.xlsx'
+                    if not os.path.exists(summary_filename):
+                        continue
+                    summary_excel = pd.read_excel(summary_filename, engine='openpyxl')
+                    if 'background' in summary_excel.columns:
+                        background_col_num = summary_excel.columns.get_loc('background')
+                        end_index = summary_excel.columns.get_loc('organs_sum')
+                        for organ in summary_excel.columns[background_col_num + 1 : end_index]:
+                            print(organ)
+                            organ_name = organ.lower()
+                            organ_col_num = summary_excel.columns.get_loc(organ)
+                            organ_label = organ_col_num - background_col_num
+                            organ_col = summary_excel[organ]
+                            for index, id in enumerate(summary_excel['ids']):
+                                if organ_col[index] == 1:
+                                    if id.startswith('LITS_test-volume'):
+                                        file = dir + '/labels/' + id + '_0000.nii.gz'
+                                    else:
+                                        file = dir + '/labels/' + id + '.nii.gz'
+                                    file_path = os.path.join(root, file)
+                                    new_root = root.replace(dataset_name, new_dataset_name)
+                                    new_root = new_root.replace('CACTS', 'CACTS/'+organ_name)
+                                    base, extension = os.path.splitext(file)
+                                    base2, extension2 = os.path.splitext(base)
+                                    new_filename = f"{base2}_{organ_name}{extension2}{extension}"
+                                    new_file_path = os.path.join(new_root, new_filename)
+                                    if os.path.exists(new_file_path):
+                                        print(new_file_path)
+                                    else:
+                                        extract_label(organ_label, file_path, new_file_path)
